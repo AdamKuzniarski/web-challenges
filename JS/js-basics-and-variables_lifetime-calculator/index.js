332e994bb486ed379039996ccf4160a450b7ddab
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-11 12:19:42 +0200

    change sleepHour variable from 8.5 to 8 hours

--- JS/js-basics-and-variables_lifetime-calculator/index.js
+++ JS/js-basics-and-variables_lifetime-calculator/index.js
@@ -20,7 +20,7 @@ let age = 36;
 const dayOfYear = 365;
 const lifeSpan = 80;
 const hoursDay = 24;
-const sleepHours = 8.5;
+const sleepHours = 8;
 
 let totalNumbersOfDays = age * dayOfYear;
 const estimatedNumberOfDaysRemain = Math.round(
