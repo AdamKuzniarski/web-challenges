8d5b2efccdbbdbda8b2f527b506f08b8987ef34d
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-12 11:19:48 +0200

    implement localStorage

--- JS/js-dom-and-events_dark-mode/js/index.js
+++ JS/js-dom-and-events_dark-mode/js/index.js
@@ -5,20 +5,27 @@ const darkModeButton = document.querySelector("[data-js=dark-mode-button]");
 const lightModeButton = document.querySelector("[data-js=light-mode-button]");
 const toggleButton = document.querySelector("[data-js=toggle-button]");
 
-darkModeButton.addEventListener("click", () => {
+// Beim Laden prüfen, ob Dark Mode gespeichert ist
+if (localStorage.getItem("darkMode") === "enabled") {
   bodyElement.classList.add("dark");
+}
+
+/* darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  localStorage.setItem("darkMode", "enabled");
 });
 
 lightModeButton.addEventListener("click", () => {
   bodyElement.classList.remove("dark");
-});
+  localStorage.setItem("darkMode", "disabled");
+}); */
 
 toggleButton.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
+  // Status speichern
+  if (bodyElement.classList.contains("dark")) {
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+  }
 });
-
-
-let temperatur = 18
-
-const forecast = temperatur >= 5 ? 'es ist warm' : 'arschkalt';
-console.log(forecast)
