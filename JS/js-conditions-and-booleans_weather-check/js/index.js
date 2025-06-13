f4795c80fc31a94e4042a7c7554f1d689454b75e
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-13 12:40:41 +0200

    npm i download

--- JS/js-conditions-and-booleans_weather-check/js/index.js
+++ JS/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,11 @@
 // Change this value to test different "weather" conditions.
 const weather = "rainy"
 
+switch(weather){
+    case 'rainy':
+        break;
+        case
+}
+
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
 const temperature = 22; 
