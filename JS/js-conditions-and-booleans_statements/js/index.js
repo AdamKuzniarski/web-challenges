57f1a757a165aa9598e314ac246ad5e0924f552d
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-13 11:51:48 +0200

    hotdogs prices challenge solve

--- JS/js-conditions-and-booleans_statements/js/index.js
+++ JS/js-conditions-and-booleans_statements/js/index.js
@@ -5,22 +5,31 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
-if(receivedPassword === SUPER_SECRET_PASSWORD){
-    console.log('Welcome')
-}else{
-    console.log('Access deneid!')
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome");
+} else {
+  console.log("Access deneid!");
 }
 
 // Part 2: Even / Odd
-const number = 6.0; 
+const number = 6.0;
 if (number % 2 === 0) {
-    console.log('is even')
+  console.log("is even");
 } else {
-    console.log('odd!')
+  console.log("odd!");
 }
 
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 99;
+if (numberOfHotdogs > 1000000) {
+  console.log("the price 0.10€");
+} else if (numberOfHotdogs < 1000000 && numberOfHotdogs > 100) {
+  console.log("the price 1€");
+} else if (numberOfHotdogs < 100 && numberOfHotdogs > 5) {
+  console.log("the price 1.50€");
+} else {
+  console.log("the price 2€");
+}
 
 // Part 4: Daytime
 const currentHour = 12;
