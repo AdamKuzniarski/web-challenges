f84ecc8056880b33d6a100abae79be719bedf127
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-13 11:44:17 +0200

    part 1 and 2 solved

--- JS/js-conditions-and-booleans_statements/js/index.js
+++ JS/js-conditions-and-booleans_statements/js/index.js
@@ -5,8 +5,19 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if(receivedPassword === SUPER_SECRET_PASSWORD){
+    console.log('Welcome')
+}else{
+    console.log('Access deneid!')
+}
+
 // Part 2: Even / Odd
-const number = 6;
+const number = 6.0; 
+if (number % 2 === 0) {
+    console.log('is even')
+} else {
+    console.log('odd!')
+}
 
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
