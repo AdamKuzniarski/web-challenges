425ae5764e2d3547c3d74dbfb370584960c8fc32
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-06-17 16:20:50 +0200

    solution checkboxes

--- JS/js-forms_checkbox-input/js/index.js
+++ JS/js-forms_checkbox-input/js/index.js
@@ -12,13 +12,29 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+tosCheckbox.addEventListener("change", () => {
+  console.log(tosCheckbox.checked)
+  
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
 
+  if (tosCheckbox.checked) {
+    console.log("checked");
+    alert("Form submitted");
+  } else {
+    console.log("not checked");
+  }
+
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
 });
