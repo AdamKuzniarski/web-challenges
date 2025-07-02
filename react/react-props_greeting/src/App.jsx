786120d81231ce97b753f9aa6442494795531bc6
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-02 15:20:43 +0200

    solution Greeting

--- react/react-props_greeting/src/App.jsx
+++ react/react-props_greeting/src/App.jsx
@@ -1,3 +1,14 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Greetring name={"Adam"} isCoach={false} />
+      <Greetring name={"Ryba"} isCoach={false} />
+      <Greetring name={"Rybus"} isCoach={false} />
+      <Greetring name={"Flo"} isCoach />
+    </>
+  );
+}
+
+function Greetring({ name, isCoach }) {
+  return <p> {isCoach ? <p>Coach {name} </p> : <p>Hello {name}</p>}</p>;
 }
