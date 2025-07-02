8a5a6f8ae785a01a6c5f59d0fecd62ce1d4f8174
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-02 11:30:52 +0200

    solution attributes

--- react/react-basics_attributes/src/App.jsx
+++ react/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return (
+    <article>
+      <h2 className="article__title">Heading H2</h2>
+      <form action="">
+        <label className="label" id="text1" htmlFor="text1">
+          Add your text
+        </label>
+        <input htmlFor="text1" type="text" />
+      </form>
+      <a className="article__link" href="http://wikipedia.de">
+        Wikepedia
+      </a>
+    </article>
+  );
 }
