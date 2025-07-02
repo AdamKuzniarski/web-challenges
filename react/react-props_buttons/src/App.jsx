aa068caf240cef328e8032ff25c17762953dce15
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-02 17:49:41 +0200

    initial commit buttons

--- react/react-props_buttons/src/App.jsx
+++ react/react-props_buttons/src/App.jsx
@@ -1,20 +1,3 @@
 export default function App() {
-  const handleClick =()=>{
-    alert('Clicked')
-    
-  }
-  return (
-    <>
-      <Button text={"yes"} color={"red"} disabled={false} onClick={handleClick} />
-      <Button text={"no"} disabled={true} color={"blue"} />
-    </>
-  );
-}
-
-function Button({ color, disabled, text, onClick }) {
-  return (
-    <button  disabled={disabled} style={{ backgroundColor:  color  }} onClick={onClick}   >
-      {text}
-    </button>
-  );
+  return <h1>Replace me with your component!</h1>;
 }
