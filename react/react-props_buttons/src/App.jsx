1fd66c52990b4b9bd018082ef646c46b2cb7e0bf
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-05 15:55:51 +0200

    solution button

--- react/react-props_buttons/src/App.jsx
+++ react/react-props_buttons/src/App.jsx
@@ -1,3 +1,12 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+
+  function wypuscLog(){
+    console.log('Rybitwa')
+  }
+
+  return <Button kolorTekstu='red' textWewnatrz='Rybitwa' jestKlikalny={false} funkcja={wypuscLog}/>
+}
+
+function Button({textWewnatrz,kolorTekstu, jestKlikalny, funkcja }){
+  return <button style={{color: kolorTekstu}} disabled={jestKlikalny} onClick={funkcja} >{textWewnatrz}</button>
 }
