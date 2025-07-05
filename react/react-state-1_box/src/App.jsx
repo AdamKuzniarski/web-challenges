a5fef36660ac2749b73934c1e4dc8c0fc2ad3646
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-05 17:37:44 +0200

    solution box

--- react/react-state-1_box/src/App.jsx
+++ react/react-state-1_box/src/App.jsx
@@ -1,18 +1,41 @@
 import "./styles.css";
-
+import { useState } from "react";
 export default function App() {
-  let isActive = false;
+  const [isActive, setActive] = useState(false)
 
   function handleClick() {
-    isActive = !isActive;
-    // Check that the value changes correctly.
-    console.log(isActive);
+    setActive(!isActive)
+
   }
 
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>{isActive ? 'Activate':'Deactivate'}</button>
     </main>
   );
 }
+/* 
+# React State 1: Box
+
+
+- Replace the `let` variable with the `useState` function.
+  - Don't forget to import `useState` from React.
+  - Set the initial state value to `false`.
+- Inside the `handleClick` function, use the `set` function from `useState` to update the value of the `isActive` state.
+
+Once you’ve implemented these changes, you should be able to toggle the box’s color using the button! 🎉
+
+### Bonus
+
+Can you use the new state to toggle the button text between “Activate” and “Deactivate”?
+
+### Hacker Bonus
+
+Check the console and pay close attention to the logged value of `isActive`. You will notice that `console.log()` does not immediately show the value you just set with the `set` function one line before. For example, if you set `isActive` to `true`, it will log `false` instead, and vice versa.
+
+This is not a bug - it is the expected behavior due to how React’s state works fundamentally.
+
+Can you explain why this happens?
+
+*/
