e0c4a41a12b08c6109e09c1867b85a646bd829fe
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-10 12:47:59 +0200

    solution

--- react/react-state-2_personal-details/src/App.jsx
+++ react/react-state-2_personal-details/src/App.jsx
@@ -1,17 +1,30 @@
 import "./styles.css";
 import Form from "./components/Form";
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const name = formData.get("name");
+    const email = formData.get("email");
+    setName(name);
+    setEmail(email);
+    event.target.reset();
+  }
   return (
     <div className="container">
       <h1>Personal Details Form</h1>
-      <Form />
+      <Form onSubmit={handleSubmit} />
       <h2>Your submitted details:</h2>
       <p>
-        Name: <span className="output">John Doe</span>
+        Name: <span className="output">{name}</span>
       </p>
       <p>
-        Email: <span className="output">John@Doe.com</span>
+        Email: <span className="output">{email}</span>
       </p>
     </div>
   );
