0aa3c82065d68ba83ae19d1c7fd1c0a127cd8aca
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-22 18:42:32 +0200

    solution switching

--- nextJS/react-global-state_lights-1/components/Light/index.js
+++ nextJS/react-global-state_lights-1/components/Light/index.js
@@ -1,18 +1,14 @@
-import { useState } from "react";
+
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
-export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
-
-  function handleToggle() {
-    setIsOn((isOn) => !isOn);
-  }
+export default function Light({ name, isOn, toggleLight, id }) {
+  function handleToggle() {}
 
   return (
     <LightButton
       type="button"
       onClick={() => {
-        handleToggle();
+        toggleLight(id);
       }}
       $isOn={isOn}
     >
--- nextJS/react-global-state_lights-1/pages/_app.js
+++ nextJS/react-global-state_lights-1/pages/_app.js
@@ -1,11 +1,48 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const initialLights = [
+    { id: 1, name: "LivingRoom", isOn: false },
+    {
+      id: 2,
+      name: "Kitchen",
+      isOn: false,
+    },
+    {
+      id: 3,
+      name: "Bedroom",
+      isOn: false,
+    },
+    {
+      id: 4,
+      name: "Bathroom",
+      isOn: false,
+    },
+    {
+      id: 5,
+      name: "Garage",
+      isOn: false,
+    },
+    { id: 6, name: "Garden", isOn: false },
+    { id: 7, name: "Office", isOn: false },
+  ];
+
+  const [light, setLight] = useState(initialLights);
+
+  function toggleLight(id) {
+    setLight(
+      light.map((singleLight) => 
+          singleLight.id === id ? {... singleLight, isOn: !singleLight.isOn} : singleLight
+        
+      ))
+  }
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} light={light} toggleLight={toggleLight} />
     </Layout>
   );
 }
