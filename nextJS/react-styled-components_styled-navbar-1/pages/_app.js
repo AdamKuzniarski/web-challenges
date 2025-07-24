81430dbc6c1045e792bba642cf778f8519960c68
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-07-24 13:19:21 +0200

    solution

--- nextJS/react-styled-components_styled-navbar-1/pages/_app.js
+++ nextJS/react-styled-components_styled-navbar-1/pages/_app.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import GlobalStyle from "../styles";
 import styled from "styled-components";
+import { useState } from "react";
 
 const StyledNav = styled.nav`
   border-bottom: solid 2px grey;
@@ -13,23 +14,38 @@ const StyledList = styled.ul`
   gap: 1rem;
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const StlyedButton = styled.button`
+  background: red;
+  padding: 50px 75px;
+  color: aliceblue;
+`
+
 export default function App({ Component, pageProps }) {
+  const [smile, setSmile] = useState(false)
   return (
     <>
       <GlobalStyle />
       <StyledNav>
         <StyledList>
           <li>
-            <Link href="/">Home</Link>
+            <StyledLink href="/">Home</StyledLink>
           </li>
           <li>
-            <Link href="/about">About</Link>
+            <StyledLink href="/about">About</StyledLink>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <StyledLink href="/contact">Contact</StyledLink>
           </li>
         </StyledList>
       </StyledNav>
+      <StlyedButton onClick={()=>setSmile(!smile)}>{smile ?"☺️" :"🙈"}</StlyedButton>
       <Component {...pageProps} />
     </>
   );
