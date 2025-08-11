ffe24a2fd6465a83b5811e025fb3c270ef6158b0
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-08-11 14:46:06 +0200

    solution

--- backend/backend-basics_routes/index.js
+++ backend/backend-basics_routes/index.js
@@ -1,3 +1,11 @@
 // import { server } from "./server.js";
 
 // server.listen(…
+
+import { server } from "./server.js";
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server läuft: http://localhost:${PORT}`);
+});
--- backend/backend-basics_routes/server.js
+++ backend/backend-basics_routes/server.js
@@ -1,3 +1,37 @@
 import { createServer } from "node:http";
 
-// export const server = …
+const fishById = {
+  1: "Shrimp",
+  2: "Anemonefish",
+  3: "Ryba",
+};
+
+/* const routes = {
+  "/api/fish/1": "Shrimp",
+  "/api/fish/2": "Anemonefish",
+}; */
+
+export const server = createServer((request, response) => {
+  response.setHeader("Content-Type", "text/plain; charset=utf-8");
+
+  const { pathname } = new URL(request.url, "http://localhost");
+
+  const match = pathname.match(/^\/api\/fish\/([^/]+)$/);
+
+  if (match) {
+    const id = match[1];
+    const name = fishById[id];
+
+    if (name) {
+      response.statusCode = 200;
+      response.end(name);
+    } else {
+      response.statusCode = 404;
+      response.end("Not found");
+    }
+    return;
+  }
+
+  response.statusCode = 404;
+  response.end("Not found");
+});
