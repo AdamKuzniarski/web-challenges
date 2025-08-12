e2a42005eb6d277be5018e3ec7b9a9ee00633df0
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-08-12 18:59:03 +0200

    solution

---
+++ backend/backend-read_products/db/models/Product.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import Review from "./Review";
+
+const { Schema } = mongoose;
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  currency: String,
+  reviews: { type: [Schema.Types.ObjectId], ref: Review },
+});
+
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
